83e413f0793f5ded30d83bebcc417b27e6c9cabe
Pavol Mráz <pavol.mraz@fores.group>
2025-08-17 11:20:54 +0200

    Add project files.

---
+++ Net7ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Net7ConsoleApp;
+
+internal class Program
+{
+  static async Task Main(string[] args)
+  {
+    Console.WriteLine("Press ENTER to call async service");
+    Console.ReadLine();
+    await PerformServiceTaskAsync();
+  }
+
+
+  static async Task PerformServiceTaskAsync()
+  {
+    await using var service = new Netstandard20ClassLibrary.Service();
+    await service.PerformServiceAsync();
+  }
+}
---
+++ Netstandard20ClassLibrary/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Netstandard20ClassLibrary;
+
+public class Service : IAsyncDisposable
+{
+  public Service() { }
+
+
+  public ValueTask DisposeAsync()
+  {
+    return new ValueTask(Task.CompletedTask);
+  }
+
+
+  public Task PerformServiceAsync()
+  {
+    return Task.CompletedTask;
+  }
+
+}
